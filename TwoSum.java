876e13655053585b637027a1267ed01da11296ef
Rashmi Kumari <rrashmikk@gmail.com>
2016-02-07 01:34:41 -0500

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -3,18 +3,13 @@ public class Solution {
         int[] result = {0,0};
         int size = nums.length;
         for (int i = 0; i < size; i++){
-        	if (nums[i] < target){
-        		for (int j = i+1; j < size; j++){
-        			if (nums[j] < target){
-        				if (nums[i]+nums[j] == target){
-        					result[0] = i+1;
-        					result[1] = j+1;
-        					break;
-        				}
-        			}
-        		}
-        	}
-        }
+			for (int j = i + 1; j < size; j++) {
+				if (nums[i] + nums[j] == target) {
+					result[0] = i + 1;
+					result[1] = j + 1;
+				}
+			}
+		}
         return result;
     }
 }
